1a421bf95cfbb2665412e4af8281e5a542f02717
Michael Tomae <michael.tomae@gmail.com>
2018-08-22 00:19:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,5 +7,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const body = document.querySelctor('body')
-
+  body.addEventListener('keydown', (event) => {
+    if(event.which ===
+  })
 }
