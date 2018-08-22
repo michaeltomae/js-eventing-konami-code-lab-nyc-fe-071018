1bf4da7ab191a14681854d53c85cca64e31e6246
Michael Tomae <michael.tomae@gmail.com>
2018-08-22 00:53:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,12 +7,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let tracker = 0
 function onKeyDownHandler(event) {
+      const key = parseInt(event.detail || event.which);
       if (tracker === code.length){
       window.alert('You found it!')
       event.preventDefault()
       return
     }
-    if (event.which === code[tracker]) {
+    if (key === code[tracker]) {
       tracker++
       if (tracker === code.length) {
         window.alert('You found it!')
