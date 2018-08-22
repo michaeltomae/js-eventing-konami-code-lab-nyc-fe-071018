bff5e4600550b4d59d01a3d96f4b0856bf6b22fd
Michael Tomae <michael.tomae@gmail.com>
2018-08-22 00:29:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-// Up Up Down Down Left Right Left Right B 
+// Up Up Down Down Left Right Left Right B A
 // We have an array of key codes (event.which)
 // Add an event listener somewhere so that when we press this, something happens
 // We need to keep track of where we are in the combo
